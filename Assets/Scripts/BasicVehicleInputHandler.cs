e693b867190620f941d8bdc31b2ad09a43c4d1cc
William Xie <williamxie168@berkeley.edu>
2021-08-05 00:39:39 +0800

    Basic scripts done

---
+++ Assets/Scripts/BasicVehicleInputHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicVehicleInputHandler : MonoBehaviour {
+	
+	public BasicVehicleMotor motor;
+
+	private void Awake() {
+		if (motor == null) motor = GetComponent<BasicVehicleMotor>();
+	}
+
+	private void Update() {
+		motor.accelerationInput = Input.GetAxis("Vertical");
+		motor.steeringInput = Input.GetAxis("Horizontal");
+	}
+}
---
+++ Assets/Scripts/BasicVehicleMotor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BasicVehicleMotor : MonoBehaviour {
+
+	public float maxSpeed;
+	public float accelerationPower;
+	public float maxBrakePower;
+	public float maxBrakeTime;
+	public float steeringPower;
+	public float driftPower;
+	public float minSpeedToRotate;
+	public float softRotationInertia = 8;
+
+	public float accelerationInput;
+	public float steeringInput;
+	
+	public Rigidbody2D rigidbody;
+
+	public Vector2 currentVelocity;
+	public float rotation;
+
+	private void Awake() {
+		if (rigidbody == null) rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	public void FixedUpdate() {
+		UpdateEngine();
+		AdjustDrift();
+		UpdateSteering();
+	}
+
+	private void UpdateEngine() {
+		float forwardSpeed = Vector2.Dot(transform.up, rigidbody.velocity);
+		if (accelerationInput > 0 && rigidbody.velocity.sqrMagnitude >= maxSpeed * maxSpeed) return;
+		if (accelerationInput > 0 && forwardSpeed >= maxSpeed) return;
+		if (accelerationInput < 0 && forwardSpeed <= -maxSpeed) return;
+
+		if (accelerationInput == 0) rigidbody.drag = Mathf.MoveTowards(rigidbody.drag, maxBrakePower, Time.fixedDeltaTime * maxBrakeTime);
+		else rigidbody.drag = 0;
+		
+		Vector2 engineForce = transform.up * (accelerationInput * accelerationPower);
+		rigidbody.AddForce(engineForce, ForceMode2D.Force);
+	}
+
+	private void UpdateSteering() {
+		float speed = rigidbody.velocity.magnitude;
+		float rotationInertiaFactor = speed <= minSpeedToRotate ? 0 : speed / softRotationInertia;
+		rotationInertiaFactor = Mathf.Clamp01(rotationInertiaFactor);
+
+		rotation -= steeringInput * steeringPower * rotationInertiaFactor;
+		rigidbody.MoveRotation(rotation);
+	}
+
+	private void AdjustDrift() {
+		Vector2 forwardVelocity = transform.up * Vector2.Dot(rigidbody.velocity, transform.up);
+		Vector2 rightVelocity = transform.right * Vector2.Dot(rigidbody.velocity, transform.right);
+
+		rigidbody.velocity = forwardVelocity + rightVelocity * driftPower;
+	}
+}
