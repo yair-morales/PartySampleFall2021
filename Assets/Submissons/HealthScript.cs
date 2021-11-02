6b793a627233a4d2d85bf8c026284bcdea9aac63
Yair Morales <yair-morales@berkeley.edu>
2021-11-01 20:25:07 -0700

    Health script

---
+++ Assets/Submissons/HealthScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public int HP;
+    public Action onDeathAction;
+    public bool dead; 
+    public int maxHP;
+
+    public void OnDamageTaken(int dmg, Vector3 dir)
+    {
+        if (dmg < 0 || dead) return;
+        HP -= dmg;
+        if (HP <= 0)
+        { 
+            onDeath?.Invoke();
+            dead = true;
+        }
+    }
+
+    public void OnHealthRestore(int heal, Vector3 dir)
+    {
+        if (HP >= maxHP) return;
+        if (HP + heal > maxHP)
+        {
+            HP = maxHP;
+        }
+        else
+        {
+            HP += heal;
+        }
+    }
+}
