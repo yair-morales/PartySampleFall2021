eccb720f90ff4e4241785b5ff50ac9701a0dcd8e
William Xie <williamxie168@berkeley.edu>
2021-11-01 21:08:21 -0700

    Update

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioClip bgm;
+	
+	public void Awake() {
+		var audioSource = GetComponent<AudioSource>();
+		if (audioSource == null) {
+			audioSource = gameObject.AddComponent<AudioSource>();
+		}
+
+		audioSource.clip = bgm;
+		
+		if (audioSource.clip) audioSource.Play();
+		
+		DontDestroyOnLoad(gameObject);
+	}
+}
