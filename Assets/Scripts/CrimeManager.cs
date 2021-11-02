132b3536457dbefa48f4339b6dc1da3c73e516cf
William Xie <williamxie168@berkeley.edu>
2021-11-01 22:02:39 -0700

    CrimeManager

---
+++ Assets/Scripts/CrimeManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class CrimeManager : MonoBehaviour {
+
+	public void OnCrimeHappen(Vector3 position, Vector3 criminalDirection) {
+		
+	}
+}
