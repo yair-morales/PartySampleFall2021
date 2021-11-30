3fd3adee06032d6f57abbadbd2fe6c7c02955f14
Yair Morales <yair-morales@berkeley.edu>
2021-11-29 21:58:03 -0800

    AudioLord
    Audio manager singleton

---
+++ Assets/Scripts/AudioLord.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AudioLord : MonoBehaviour
+{
+    public AudioClip music;
+    private void Awake()
+    {
+        Instance.clip = music;
+        Instance.Play();
+        Instance.loop = true;
+    }
+
+    public static AudioSource Instance
+    {
+        get
+        {
+            if (!bgm)
+            {
+                GameObject audioLord = new GameObject("audioLord");
+                bgm = audioLord.AddComponent<AudioSource>();
+                DontDestroyOnLoad(audioLord);
+            }
+
+            return bgm;
+        }
+    }
+
+    public static GameObject singletonRoot;
+    private static AudioSource bgm;
+
+    public static void AudioPlay()
+    {
+
+    }
+}
